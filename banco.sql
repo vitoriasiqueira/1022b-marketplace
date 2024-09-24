dce7023f85519d19b3acae543bdb9dca3cd4dbb7
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-09-24 07:46:57 -0400

    Adicionando o SQL do banco de dados.

---
+++ banco.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS banco1022b;
+CREATE DATABASE banco1022b;
+USE banco1022b;
+CREATE TABLE IF NOT EXISTS produtos(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    descricao VARCHAR(50),
+    preco DECIMAL(10,2),
+    imagem VARCHAR(300)
+);
+INSERT INTO produtos VALUES (1,"Iphone","Celular RUIM",5000.50,"SEM IMAGEM");
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-console.log("TERE LINDO sz");
+// LIVE SERVER é do FRONT-END
+// QUEM É O LIVE SERVER DO BACK-END?
+
+// 1 - Para construir um servidor back-end e responder
+// Vamos utilizar o EXPRESS
+import express from 'express'
+import cors from 'cors'
+//Criar um objeto do tipo express.
+const app = express()
+//incluir pra ele receber json
+app.use(express.json())  //Middleware
+//incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
+app.use(cors())
+//ROTAS
+app.get("/produtos",(req,res)=>{
+
+    //O que eu tenho que fazer aqui dentro?
+    //PASSO 1: Criar o banco de dados
+    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+    //PASSO 3: QUERY  -> SELECT * FROM produtos
+    //PASSO 4: Colocar os dados do banco no response
+    res.send({mensagem:"Eu sou a rota de produtos"})
+})
+
+
+//INICIAR O SERVIDOR
+app.listen(8000,()=>{
+    console.log("SERVIDOR INICIADO NA PORTA 8000")
+})
