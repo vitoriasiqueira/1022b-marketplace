a06f6794c16179870c49d4e428bb840d1edaf0fe
vitoriasiqueira <vitoria.siqueira@estudante.ifms.edu.br>
2024-10-22 08:09:49 -0400

    Alterações

--- banco.sql
+++ banco.sql
@@ -7,3 +7,13 @@ CREATE TABLE IF NOT EXISTS produtos(
     imagem VARCHAR(300)
 );
 INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+
+USE defaultdb;
+CREATE TABLE IF NOT EXISTS usuarios (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(50),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+INSERT INTO usuarios VALUES (1, 'Bruna', 'brunasilva@estudante.ifms.edu.br', '2024-10-22 07:12:00', '2024-10-22 07:13:00' );
